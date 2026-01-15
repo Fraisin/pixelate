157023df6b40170ee8cb857e773b39feb81dcf0a
Frank Yi <frank.yi@coinbase.com>
2026-01-15 09:20:08 -0500

    hello world

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-[#0052FF]">
+      <h1 className="text-4xl font-bold text-white">Pixelate</h1>
+    </main>
+  );
+}
+
